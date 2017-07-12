a6dc9f34a786e28c0e0bbb10e19c2950cbccdaa3
ivan <mpeg_4@bk.ru>
2017-07-12 22:43:35 +0800

    :fire: Начало, ёба!

---
+++ lib/db.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ivan
+ * Date: 12.07.17
+ * Time: 21:37
+ */
+
+global $db;
+
+$db = new mysqli('localhost', 'root', 'root', 'blog');
---
+++ lib/functions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ivan
+ * Date: 12.07.17
+ * Time: 21:42
+ */
+
+function getCategory()
+{
+    global $db;
+
+    $query = $db->query('SELECT * FROM');
+}
